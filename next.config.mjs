b2fa75531f1d3713808ce69b93c0c8114d7a37b4
romanyrefaat1 <refaatromany641@gmail.com>
2025-07-21 12:58:29 +0300

    Ignore eslint and typescript when building

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ["fakestoreapi.com", "i.pravatar.cc"], // Allow images from fakestoreapi.com
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARNING !! This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
