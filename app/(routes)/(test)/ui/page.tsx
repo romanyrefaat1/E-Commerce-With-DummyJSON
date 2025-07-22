07b90483964d40f5a492e15b0eb5f4c5a3495dca
romanyrefaat1 <refaatromany641@gmail.com>
2025-07-22 04:30:30 +0300

    DaisyUI and TailwindCSS Setup finished

--- app/(routes)/(test)/ui/page.tsx
+++ app/(routes)/(test)/ui/page.tsx
@@ -1,7 +1,9 @@
 export default function TestUIPage() {
   return (
     <div className="p-4 bg-red-500 w-screen">
-      <button className="btn btn-primary">Click me</button>
+      <button className="btn btn-primary hover:text-background">
+        Click me
+      </button>
       {/* <button className="btn btn-secondary ml-2">Secondary</button> */}
     </div>
   );
