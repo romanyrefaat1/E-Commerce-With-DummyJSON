1f900636723558f83ec69f3fb155cf81fe4e8b7f
romanyrefaat1 <refaatromany641@gmail.com>
2025-07-22 04:57:14 +0300

    Create button micro interaction

--- app/(routes)/(test)/ui/page.tsx
+++ app/(routes)/(test)/ui/page.tsx
@@ -1,8 +1,17 @@
 export default function TestUIPage() {
+  console.log(1 / 0.93);
   return (
     <div className="p-4 bg-red-500 w-screen">
-      <button className="btn btn-primary hover:text-background">
-        Click me
+      <button
+        className="transition duration-150 btn btn-secondary group hover:scale-[.96] hover:rounded-box
+      active:scale-[.93]"
+      >
+        <span
+          className="transition duration-150 group-hover:opacity-90 group-hover:scale-[1.0416]
+        group-active:scale-[1.0752]"
+        >
+          Click me
+        </span>
       </button>
       {/* <button className="btn btn-secondary ml-2">Secondary</button> */}
     </div>
