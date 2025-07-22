a441b3e07e5089989be4548788420cfd96405865
romanyrefaat1 <refaatromany641@gmail.com>
2025-07-22 03:43:16 +0300

    Daisy and tailwind work properly but with old versions

--- app/(routes)/(test)/ui/page.tsx
+++ app/(routes)/(test)/ui/page.tsx
@@ -1,6 +1,6 @@
 export default function TestUIPage() {
   return (
-    <div className="p-4 bg-red-500">
+    <div className="p-4 bg-red-500 w-screen">
       <button className="btn btn-primary">Click me</button>
       {/* <button className="btn btn-secondary ml-2">Secondary</button> */}
     </div>
